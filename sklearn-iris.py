f71113a9d8e6638443a216ad009c7d4a37acc3a4
Tsuen86 <72797003+Tsuen86@users.noreply.github.com>
2022-04-02 18:46:19 +0800

    Update sklearn-iris.py

--- sklearn-iris.py
+++ sklearn-iris.py
@@ -32,7 +32,7 @@ if readme:
         \n * Scikit Learn Model: Logistic Regression
         \n * Host & Libraries: [Streamlit](https://streamlit.io/)
         \n * Code Repository: [Github](https://github.com/Tsuen86/iris-classification/)
-        \n <html> <hr>""")
+        \r\n """)
 
 st.sidebar.write("""
 This is a web app to predict the probably species of Iris flower based on its sepal and petal dimensions (length, width)
